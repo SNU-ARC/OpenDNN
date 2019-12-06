d50bcdee8208a0f2917c0f98fdc4122f53998f45
Young H. Oh <garion9013@gmail.com>
2019-12-06 20:47:25 +0900

    target patch option

--- examples/download_and_patch_caffe.sh
+++ examples/download_and_patch_caffe.sh
@@ -4,4 +4,15 @@ git clone https://github.com/BVLC/caffe
 cd caffe
 git checkout 04ab089d 
 cp ../../include/opendnn.h include/caffe
-patch -p1 < ../opendnn-to-caffe.patch
+
+target=$1
+if [ $target == cuda ]; then
+  echo "Patch CUDA version of Caffe"
+  patch -p1 < ../opendnn-to-caffe.patch
+elif [ $target == ocl ]; then
+  echo "Patch OpenCL version of Caffe"
+  patch -p1 < ../opendnn-to-caffe-ocl.patch
+else
+  echo "Patch CPU version of Caffe"
+  patch -p1 < ../opendnn-to-caffe-ocl.patch
+fi
